681c487c5c14e30d3dc97f85ddb3849bf27b4679
Javier Bullrich <javier@bullrich.dev>
2023-05-02 10:08:51 +0200

    set dark mode by default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 module.exports = {
     content: ['./src/**/*.{njk,md,html}'],
     plugins: [require('daisyui'), require('@tailwindcss/typography')],
+    daisyui: {
+      themes: ["dark"]
+    }
   };
